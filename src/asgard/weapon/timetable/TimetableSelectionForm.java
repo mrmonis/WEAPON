317365cf8511da6b249c556638f1d813bedbaecc
Benjamin Monis <slowmonis@gmail.com>
2012-03-11 20:27:53 -0400

    Working on timetable select and delete

--- src/asgard/weapon/timetable/TimetableSelectionForm.java
+++ src/asgard/weapon/timetable/TimetableSelectionForm.java
@@ -1,13 +1,20 @@
 package asgard.weapon.timetable;
 
+import java.util.ArrayList;
+
+import asgard.weapon.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
 import asgard.weapon.ConditionCodes;
-import asgard.weapon.R;
 
 /**
  * 
@@ -22,19 +29,25 @@ public class TimetableSelectionForm extends Activity implements OnClickListener,
 	private TimetableController mController;
 
 	private Handler mHandler;
+	
+	private ArrayList<Timetable> mTimetables;
+	
+	private RelativeLayout mView;
+	private boolean delete;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.timetable_selection_form);
+		//setContentView(R.layout.timetable_selection_form);
 
+		delete = false;
 		mHandler = new Handler(this);
-
-		//mName = (EditText) findViewById(R.id.timetable_creation_form_edit_text);
 
 		mController = TimetableController.getController(this);
 		mController.addHandler(mHandler);
+		
+		mController.getHandler().obtainMessage(ConditionCodes.V_GET_ALL_TIMETABLE).sendToTarget();
 	}
 
 	@Override
@@ -42,19 +55,57 @@ public class TimetableSelectionForm extends Activity implements OnClickListener,
 		Handler handler = mController.getHandler();
 		Message message = handler.obtainMessage(ConditionCodes.V_DO_NOTHING,
 				this);
-
-		switch (v.getId()) {
-		case R.id.timetable_creation_form_create_button:
-			message.what = ConditionCodes.V_CREATE_TIMETABLE;
-
-			break;
-			
-		case R.id.timetable_creation_form_new_course_button:
-			message.what = ConditionCodes.V_LAUNCH_COURSE_CREATION_FORM;	
-			handler.dispatchMessage(message);
-			break;
+		
+		int position = v.getId();
+		
+		Message msg = mController.getHandler().obtainMessage();
+		
+		if (delete){
+			msg.what = ConditionCodes.V_DELETE_SELECTED;
 		}
+		else{
+			msg.what = ConditionCodes.V_TIMETABLE_SELECTED;
+		}
+		msg.arg1 = position;
+		msg.sendToTarget();
+		finish();
+		//test
 
+	}
+	
+	public void initialize(){
+		//mView = (RelativeLayout) findViewById(R.id.button_container);
+		LayoutInflater li = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+		
+		for (int i = 0; i < mTimetables.size(); i++){
+			
+			RelativeLayout rl = (RelativeLayout) li.inflate(
+					R.layout.button_form, null);
+			
+			Button temp = (Button) rl.findViewById(R.id.button);
+			TextView name = (TextView) rl.findViewById(R.id.text_view);
+			temp.setOnClickListener(this);
+			temp.setText(mTimetables.get(i).getName());
+			temp.setId(i);
+			
+			LayoutParams buttonLP = (LayoutParams) temp.getLayoutParams();
+			
+			if (i != 0) {
+				Button aboveButton = (Button) findViewById(i-1);
+				buttonLP.addRule(RelativeLayout.BELOW, aboveButton.getId());
+				temp.setLayoutParams(buttonLP);
+
+			} else {
+				buttonLP.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+				temp.setLayoutParams(buttonLP);
+			}
+
+			
+
+			rl.removeAllViews();
+			mView.addView(temp);
+		}		
+		
 	}
 
 	@Override
@@ -66,7 +117,15 @@ public class TimetableSelectionForm extends Activity implements OnClickListener,
 
 	@Override
 	public boolean handleMessage(Message msg) {
-		// TODO Auto-generated method stub
+		switch (msg.what){
+		case ConditionCodes.C_SEND_ALL_TIMETABLE:
+			mTimetables = (ArrayList<Timetable>) msg.obj;
+			if (msg.arg1 != 0){
+				delete = true;
+			}
+			initialize();
+			break;
+		}
 		return false;
 	}
 }
